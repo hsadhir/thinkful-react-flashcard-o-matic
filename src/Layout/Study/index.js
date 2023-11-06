6f988f6d6d34fa667ea49df0096747e88553da88
Hasan Sadhir Mohammed Mansoor <hsadhir@indeed.com>
2023-11-06 13:16:05 +0530

    Added study feature & qualified tests passed

--- src/Layout/Study/index.js
+++ src/Layout/Study/index.js
@@ -5,10 +5,30 @@ import { readDeck } from "../../utils/api";
 import Navigation from "../Navigation";
 import NotEnoughCards from "./NotEnoughCards";
 
+import "./Study.css";
+
 export default function Study() {
   const { deckId } = useParams();
   const [loading, setLoading] = useState(false);
   const [deck, setDeck] = useState(null);
+
+  const [back, setBack] = useState(false);
+  const [curr, setCurr] = useState(0);
+
+  const nextHandler = () => {
+    const totalCards = deck.cards.length;
+    if (curr === totalCards - 1) {
+      const userAction = window.confirm(`Restart Cards ?`);
+
+      if (userAction) {
+        setCurr(0);
+        setBack(!back);
+      }
+    } else {
+      setCurr((curr) => curr + 1);
+      setBack(!back);
+    }
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -48,7 +68,42 @@ export default function Study() {
         />
         <section className="direct-child-container">
           {cards.length >= 3 ? (
-            <p>To be Implemented</p>
+            <div>
+              <h2 style={{ textAlign: "center" }}>Study: {deck.name}</h2>
+              <div className="study-container">
+                <h3>
+                  Card {curr + 1} of {cards.length}
+                </h3>
+                <div className={`${back ? "back-display" : "front-display"}`}>
+                  {back ? (
+                    <p>{cards[curr].back}</p>
+                  ) : (
+                    <p>{cards[curr].front}</p>
+                  )}
+                </div>
+                <div className="study-actions">
+                  {back && curr > 0 && (
+                    <button
+                      className="purple-btn"
+                      onClick={() => {
+                        setCurr((curr) => curr - 1);
+                        setBack(!back);
+                      }}
+                    >
+                      Previous
+                    </button>
+                  )}
+                  <button className="blue-btn" onClick={() => setBack(!back)}>
+                    Flip
+                  </button>
+                  {back && (
+                    <button className="green-btn" onClick={nextHandler}>
+                      Next
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
           ) : (
             <NotEnoughCards deckId={deckId} cardsCount={cards.length} />
           )}
